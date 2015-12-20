339041d631e6b08a42a16962a4c69ad7a140e199
Nathaniel Nelson <nelson.nleroy@gmail.com>
2015-12-20 13:14:14 -0700

    MailAccount can be constructed from environment variabls

--- mail_account.py
+++ mail_account.py
@@ -1,12 +1,14 @@
+import os
 import imaplib
 import smtplib
 import markdown
 from email.mime.text import MIMEText
 
 
-class MailBot(object):
-    """ Connects to an IMAP and an SMTP email server at the given locations.
-    Sends and receives email from the specified address.
+class MailAccount(object):
+    """ Connects with SSL to an IMAP and an SMTP email
+    server at the given locations.  Sends and receives email from the specified
+    address.
     """
 
     def __init__(self, imap_server, imap_port, smtp_server, smtp_port,
@@ -14,9 +16,7 @@ class MailBot(object):
         # Connect to the IMAP server
         self._imap_server = imaplib.IMAP4_SSL(imap_server, imap_port)
         # Connect to the SMTP server
-        self._smtp_server = smtplib.SMTP(smtp_server, smtp_port)
-        self._smtp_server.ehlo()
-        self._smtp_server.starttls()
+        self._smtp_server = smtplib.SMTP_SSL(smtp_server, smtp_port)
         # Log in to the email account
         self._imap_server.login(address, password)
         self._smtp_server.login(address, password)
@@ -29,6 +29,22 @@ class MailBot(object):
         self._imap_server.logout()
         # Disconnect from the SMTP server
         self._smtp_server.close()
+
+    @classmethod
+    def from_environment_vars(cls):
+        """ Construct an instance of MailAccount using a configuration
+        defined in environment variables
+        """
+
+        imap_server = os.environ['LB_IMAP_SERVER']
+        imap_port = os.environ['LB_IMAP_PORT']
+        smtp_server = os.environ['LB_SMTP_SERVER']
+        smtp_port = os.environ['LB_SMTP_PORT']
+        address = os.environ['LB_ADDRESS']
+        password = os.environ['LB_PASSWORD']
+
+        return cls(imap_server, imap_port, smtp_server, smtp_port,
+                   address, password)
 
     @property
     def imap(self):
---
+++ main.py
@@ -0,0 +1,7 @@
+from mail_account import MailAccount
+
+if __name__ == "__main__":
+    # Connect to the bot mail account
+    mail_account = MailAccount.from_environment_vars()
+
+
