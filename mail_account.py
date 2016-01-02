61addab2c24b754196d56873f44ae29cbba10f3c
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-01-01 21:33:19 -0700

    Reverting to SSL SMTP

--- mail_account.py
+++ mail_account.py
@@ -13,14 +13,10 @@ class MailAccount(object):
 
     def __init__(self, imap_server, imap_port, smtp_server, smtp_port,
                  address, password):
-        print('Connecting to IMAP')
         # Connect to the IMAP server
         self._imap_server = imaplib.IMAP4_SSL(imap_server, imap_port)
         # Connect to the SMTP server
-        print('Connecting to SMTP')
-        self._smtp_server = smtplib.SMTP(smtp_server, smtp_port)
-        self._smtp_server.ehlo()
-        self._smtp_server.starttls()
+        self._smtp_server = smtplib.SMTP_SSL(smtp_server, smtp_port)
         # Log in to the email account
         self._imap_server.login(address, password)
         self._smtp_server.login(address, password)
